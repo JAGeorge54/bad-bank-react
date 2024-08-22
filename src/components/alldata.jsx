59740bc1acb93ae0982cf43e40a6ff61373f7d3f
JAGeorge54 <jesse.george54@gmail.com>
2024-08-22 13:00:17 -0400

    added api function call for all users

--- src/components/alldata.jsx
+++ src/components/alldata.jsx
@@ -1,17 +1,32 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
+import { getUsers, getUser } from "../api";
 import UserContext from '../context/UserContext';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 
 function AllData() {
     const {users} = useContext(UserContext);
+    const [allUsers, setAllUsers] = useState();
     let name = '';
     let email = '';
     let password = '';
     let balance = '';
 
+    useEffect(() => {
+        async function loadAllUsers () {
+            let data = await getUsers();
+            if (data) {
+                setAllUsers(data)
+            }
+
+        }
+
+        loadAllUsers();
+    }, []);
+
     function Users () {
-       let card = users.map(account => {
+        let card = allUsers.map(account => {
+            console.log(allUsers);
             name = account.name;
             email = account.email;
             password = account.password;
@@ -27,6 +42,7 @@ function AllData() {
                 </Card>
             )
         })
+        console.log(card);
         return card;
     }
         
@@ -34,7 +50,8 @@ function AllData() {
     return(
         <>
             <h1>Users</h1>
-            <Users/>
+            {JSON.stringify(allUsers)}
+            {/* <Users/> */}
         </>
     )
 }
