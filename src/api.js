1b7660355fabcf4cabffa18efe9887f3d0945d91
JAGeorge54 <jesse.george54@gmail.com>
2024-08-22 13:59:38 -0400

    added delete api

--- src/api.js
+++ src/api.js
@@ -31,5 +31,6 @@ export async function updateUser (id, user) {
 }
 
 export async function deleteUser (id) {
-
+    const response = await axios.delete(`${URL}/users/:${id}`);
+    return response;
 }
