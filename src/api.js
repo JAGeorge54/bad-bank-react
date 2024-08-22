7fbe8d51cbcabdd742f83d77d9c42bfa4fe87953
JAGeorge54 <jesse.george54@gmail.com>
2024-08-22 13:16:26 -0400

    added get user api

--- src/api.js
+++ src/api.js
@@ -7,11 +7,17 @@ export async function getUsers () {
     if (response.status === 200) {
         return response.data;
     } else {
-        return response.status;
+        return console.log(`Error from API getUsers status:${response.status}`);
     }
 }
 
 export async function getUser (id) {
+    const response = await axios.get(`${URL}/users/:${id}`);
+    if (response.status === 200) {
+        return response.data;
+    } else {
+        return console.log(`Error from API getUser status:${response.status}`);
+    }
 
 }
 
