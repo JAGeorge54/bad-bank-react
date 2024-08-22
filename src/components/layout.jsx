1c50ddcd2b224e32b795140f91d1533ed24c8b78
JAGeorge54 <jesse.george54@gmail.com>
2024-08-22 15:28:30 -0400

    imported and used Navbar and outlet

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,7 +1,11 @@
+import { Navbar } from './navbar';
+import { Outlet } from 'react-router-dom';
+
 function Layout() {
     return ( 
         <>
-            
+            <Navbar />
+            <Outlet />
         </>
      );
 }
