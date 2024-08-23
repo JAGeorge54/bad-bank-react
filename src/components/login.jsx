00b29c3c2e197320d5a6ad72de47d5c10580d51f
JAGeorge54 <jesse.george54@gmail.com>
2024-08-23 12:57:38 -0400

    created login component

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,9 +1,33 @@
+import UserContext from '../context/UserContext';
+import { verifyUser } from '../api';
+import { useState } from 'react';
+
+
 function Login() {
-    return ( 
-        <>
-            <h1>Login</h1>
-        </>
-     );
+    
+    const [user, setUser] = useState({
+        email: '',
+        password: ''
+    });
+
+    function handleChange (e) {
+        setUser({ ...user, [e.target.name]: e.target.value});
+    };
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        let response = await verifyUser(user);
+        console.log(response);
+        
+    };
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <input placeholder={'Email'} onChange={handleChange} name='email' required maxLength={40}/>
+            <input placeholder={'Password'} onChange={handleChange} name='password' type='password' required maxLength={20}/>
+            <button type='submit'>Login</button>
+        </form>
+    )
 }
 
 export default Login;
