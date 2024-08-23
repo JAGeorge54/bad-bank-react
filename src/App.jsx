bd6f773e25b7a297c03dc611c8491c989e4a73c3
JAGeorge54 <jesse.george54@gmail.com>
2024-08-23 15:09:53 -0400

    added use effect to check for session storage on page refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import UserContextProvider from './context/UserContextProvider'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from 'react-router-dom'
@@ -7,14 +7,20 @@ import Home          from './pages/home'
 import AllData       from './pages/alldata'
 import Landing from './pages/landing';
 import Layout from './components/layout';
+import axios from 'axios';
 
 function App() {
   
+  useEffect(() => {
+    let token = sessionStorage.getItem("User");
+    if (token) {
+      axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    }
+  }, [])
 
   return (
     <>
       <UserContextProvider>
-        {/* <NavBar/> */}
         <Routes>
           <Route path='/' element={<Landing />}/>
           <Route element={<Layout />}>
