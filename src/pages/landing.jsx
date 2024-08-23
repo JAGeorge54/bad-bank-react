22e47928cc2eff9878fef8582ff4b0f8b31510ab
JAGeorge54 <jesse.george54@gmail.com>
2024-08-23 13:23:42 -0400

    added create account and login component to landing page, added button to toggle between pages

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,9 +1,24 @@
 import CreateAccount from "../components/createaccount";
+import Login from "../components/login";
+import { useState } from "react";
 
 function Landing() {
+
+    //view == true  --> Login
+    //view == false --> Create
+    const [ view, setView ] = useState(true);
+
     return ( 
         <>
-            <CreateAccount />
+            {!view ?
+            <>
+                <CreateAccount />
+                <button onClick={() => setView(!view)}>Login</button>
+            </> :
+            <>
+                <Login />
+                <button onClick={() => setView(!view)}>Create New Account</button>
+            </>}
         </>
      );
 }
