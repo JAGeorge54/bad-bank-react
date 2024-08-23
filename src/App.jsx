17cd06cded8a9cff09da9c4085ff1cde089c23b2
JAGeorge54 <jesse.george54@gmail.com>
2024-08-23 10:03:35 -0400

    removed login and create account, added as component

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,7 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home          from './pages/home'
 import AllData       from './pages/alldata'
-import CreateAccount from './pages/createaccount'
 import Landing from './pages/landing';
-import Login         from './pages/login'
 import Layout from './components/layout';
 
 function App() {
@@ -21,8 +19,6 @@ function App() {
           <Route path='/' element={<Landing />}/>
           <Route element={<Layout />}>
             <Route path='/home' element={ <Home/> } />
-            <Route path='/createaccount' element={ <CreateAccount/> } />
-            <Route path='/login' element={ <Login/> } />
             <Route path='/alldata' element={ <AllData/> } />
           </Route>
         </Routes>
