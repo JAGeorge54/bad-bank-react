be1c2732d8ecc64e305bf5508c129e0204a8ef19
JAGeorge54 <jesse.george54@gmail.com>
2024-08-23 15:04:42 -0400

    changed user to token

--- src/api.js
+++ src/api.js
@@ -39,8 +39,8 @@ export async function deleteUser (id) {
 export async function verifyUser (user) {
     const response = await axios.post(`${URL}/users/login`, user);
     if (response.data.success) {
-        return response.data.user
+        return response.data.token
     } else {
-        throw new Error(response.statusText)
+        return
     }
 }
