60696331c95181214d1f8c398dce3246d3918dd4
JAGeorge54 <jesse.george54@gmail.com>
2024-08-23 15:27:47 -0400

    added sessionStorage getItem to navigate back home is token is removed

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,7 +1,18 @@
 import NavBar from './navbar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function Layout() {
+
+    let user = sessionStorage.getItem("User");
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!user) {
+            navigate("/");
+        }
+    }, [user]);
+
     return ( 
         <>
             <NavBar />
