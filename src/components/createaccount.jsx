46f54b49450e1e047320b97ba91e9c93242bab49
JAGeorge54 <jesse.george54@gmail.com>
2024-08-23 10:48:10 -0400

    added form with handlesubmit and handle change functions

--- src/components/createaccount.jsx
+++ src/components/createaccount.jsx
@@ -1,13 +1,34 @@
-import UserContext from '../context/UserContext'
+import UserContext from '../context/UserContext';
+import { createUser } from '../api';
+import { useState } from 'react';
 
 
 function CreateAccount() {
     
+    const [user, setUser] = useState({
+        name: '',
+        email: '',
+        password: '',
+    });
+
+    function handleChange (e) {
+        setUser({ ...user, [e.target.name]: e.target.value});
+    };
+
+    async function handleSubmit() {
+        let response = await createUser(user);
+        if (response !== 200) {
+            alert('user account could not be created');
+        }
+    };
 
     return(
-        <>
-
-        </>
+        <form onSubmit={handleSubmit}>
+            <input placeholder={'Name'} onChange={handleChange} name='name' required maxLength={20}/>
+            <input placeholder={'Email'} onChange={handleChange} name='email' required maxLength={40}/>
+            <input placeholder={'Password'} onChange={handleChange} name='password' required maxLength={20}/>
+            <button type='submit'>Create Account</button>
+        </form>
     )
 }
 
