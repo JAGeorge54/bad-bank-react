f7c1eeb4bade2fa1adf4ccd169eb38be77b3c41f
JAGeorge54 <jesse.george54@gmail.com>
2024-08-24 10:45:28 -0400

    added use from session storage and added user information to return.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,29 @@
+import { useState, useEffect } from "react";
+import { jwtDecode } from 'jwt-decode';
+
 function Home() {
+
+    const [user, setUser] = useState({});
+
+    useEffect(() => {
+        async function loadUserData() {
+            const token = sessionStorage.getItem('User');
+            const decodedUser = jwtDecode(token);
+            setUser(decodedUser);
+        }
+        loadUserData();
+    }, [])
+
     return(
-        <h1>Home</h1>
+        <>
+            
+            <label>Name:</label>
+            <h2>{user.name}</h2>
+            <label>Email:</label>
+            <h2>{user.email}</h2>
+            <label>Balance:</label>
+            <h2>{user.balance}</h2>
+        </>
     )
 }
 
