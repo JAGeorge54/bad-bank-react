bd6102afc377bc94af404bc25cf9f92f3f85d2c6
JAGeorge54 <jesse.george54@gmail.com>
2024-08-26 08:37:37 -0400

    removed labels, added tailwind styles to h2's

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -15,15 +15,11 @@ function Home() {
     }, [])
 
     return(
-        <>
-            
-            <label>Name:</label>
-            <h2>{user.name}</h2>
-            <label>Email:</label>
-            <h2>{user.email}</h2>
-            <label>Balance:</label>
-            <h2>{user.balance}</h2>
-        </>
+        <div className="w-1/3">
+            <h2 className="flex left-0 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">{`Name: ${user.name}`}</h2>
+            <h2 className="flex left-0 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">{`Email: ${user.email}`}</h2>
+            <h2 className="flex left-0 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">{`Balance: ${user.balance}`}</h2>
+        </div>
     )
 }
 
